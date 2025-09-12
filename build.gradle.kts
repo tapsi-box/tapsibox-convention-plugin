881ed3748e2691736542a6d63db6ee2e56be13c5
Shahryar.Safizadeh <Shahryar.Safizadeh@Tapsi.cab>
2025-09-12 15:04:17 +0330

    Initiali commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    `maven-publish`
+    id("com.gradle.plugin-publish") version "1.2.1"
+}
+
+group = "box.tapsi.build"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.23.3")
+    implementation("io.gitlab.arturbosch.detekt:io.gitlab.arturbosch.detekt.gradle.plugin:1.23.5")
+    implementation("org.jetbrains.kotlinx.kover:org.jetbrains.kotlinx.kover.gradle.plugin:0.6.0")
+}
+
+gradlePlugin {
+    website = "https://github.com/tapsi-box/tapsibox-convention-plugin"
+    vcsUrl = "https://github.com/tapsi-box/tapsibox-convention-plugin.git"
+
+    plugins {
+        create("tapsiBoxConventions") {
+            id = "box.tapsi.kotlin-conventions"
+            displayName = "Tapsi Box Kotlin Conventions"
+            description = "A binary Gradle convention plugin for Tapsi Box projects that standardizes build logic with Spotless, Detekt, and Kover."
+            tags = listOf("kotlin", "convention", "spotless", "detekt", "kover")
+            implementationClass = "box.tapsi.build.TapsiBoxConventionPlugin"
+        }
+    }
+}
---
+++ src/main/kotlin/box/tapsi/build/TapsiBoxConventionExtension.kt
@@ -0,0 +1,20 @@
+package box.tapsi.build
+
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.ListProperty
+import javax.inject.Inject
+
+/**
+ * Extension exposed by the plugin so consumers can customize defaults (e.g. kover excludes).
+ * Usage in a consumer project:
+ *
+ * @author Shahryar Safizadeh
+ */
+open class TapsiBoxConventionExtension @Inject constructor(objects: ObjectFactory) {
+    val koverExcludes: ListProperty<String> = objects.listProperty(String::class.java).convention(
+        listOf(
+            "com.google.*",
+            "grpc.gateway.protoc_gen_openapiv2.options.*"
+        )
+    )
+}
